f15adac12fc5ef9028d6b626a1cccfb5b3a6b30b
Gabriele Ara <gabriele.ara@live.it>
2018-04-11 09:52:25 +0200

    Spot code updated to stay in tune with the expected new behavior

--- mote/spot_resource.c
+++ mote/spot_resource.c
@@ -9,12 +9,16 @@
 // Park Spot Resource
 /*----------------------------------------------------------------------------*/
 
-// TODO: uint8_t is too small for user probably
 struct spot_data
 {
 	uint8_t free;
-	int32_t user;
+	char user[64];
+	char credit[17];
 } status;
+
+#define STATUS_FREE		1
+#define STATUS_OCCUPIED 0
+#define STATUS_WAITING	2
 
 void spot_get_handler(void* request, void* response, uint8_t *buffer, uint16_t preferred_size, int32_t *offset);
 void spot_post_handler(void* request, void* response, uint8_t *buffer, uint16_t preferred_size, int32_t *offset);
@@ -24,22 +28,29 @@ void spot_get_handler(void* request, void* response, uint8_t *buffer, uint16_t p
 {
 	// Populate the buffer with the response payload
 	char true_false[6];
-	char usern[6];
+	char usern[68];
+	char credit[17];
 	
 	struct spot_data copy = status;
 	
-	if(copy.free)
+	if(copy.free != STATUS_OCCUPIED)
 		strcpy(true_false, "true");
 	else
 		strcpy(true_false, "false");
-		
-	if(copy.user < 0)
+	
+	if(strncmp(copy.user, "null", strlen("null")) == 0)
+	{
 		strcpy(usern, "null");
+		strcpy(credit, "null");
+	}
 	else
-		sprintf(usern, "%d", copy.user);
+	{
+		sprintf(usern, "\"%s\"", copy.user);
+		sprintf(credit, "\"%s\"", copy.credit);
+	}
 	
 	
-	sprintf((char*) buffer, "{\"free\":%s, \"user\":%s}", true_false, usern);
+	sprintf((char*) buffer, "{\"free\":%s, \"user\":%s, \"credit\":%s}", true_false, usern, credit);
 	
 	uint8_t length = strlen((char*)buffer);
 	
@@ -52,8 +63,9 @@ void spot_get_handler(void* request, void* response, uint8_t *buffer, uint16_t p
 void spot_post_handler(void* request, void* response, uint8_t *buffer, uint16_t preferred_size, int32_t *offset)
 {
 	const char* value_buffer;
+	const char* credit_buffer;
 	int8_t free_value;
-	int8_t len;
+	int8_t len, credit_len;
 	
 	len = REST.get_post_variable(request, "free", &value_buffer);
 	
@@ -67,7 +79,14 @@ void spot_post_handler(void* request, void* response, uint8_t *buffer, uint16_t 
 		return;
 	}
 	
-	free_value = !!atoi(value_buffer);
+	free_value = atoi(value_buffer);
+
+	if (free_value > 2)
+	{
+		printf("Invalid value!\n");
+		REST.set_response_status(response, REST.status.NOT_MODIFIED);
+		return;
+	}
 	
 	// Cannot push two times the same value for the free argument
 	if (free_value == status.free)
@@ -78,11 +97,11 @@ void spot_post_handler(void* request, void* response, uint8_t *buffer, uint16_t 
 	}
 	
 	// If the status is now free, nobody is using the spot, stop processing
-	if(free_value)
+	if(free_value == STATUS_FREE)
 	{
 		printf("Set to free!\n");
-		status.free = 1;
-		status.user = -1;
+		status.free = STATUS_FREE;
+		strcpy(status.user, "null");
 
 		REST.set_response_status(response, REST.status.CHANGED);
 		spot_get_handler(request, response, buffer, preferred_size, offset);
@@ -92,23 +111,58 @@ void spot_post_handler(void* request, void* response, uint8_t *buffer, uint16_t 
 		return;
 	}
 	
-	// Getting user id
+	if(free_value == STATUS_OCCUPIED)
+	{
+		if (status.free != STATUS_WAITING)
+		{
+			printf("Not modified!\n");
+			REST.set_response_status(response, REST.status.NOT_MODIFIED);
+			return;
+		}
+
+		status.free = STATUS_OCCUPIED;
+		printf("New user is %s.\n", status.user);
+		REST.set_response_status(response, REST.status.CHANGED);
+		REST.notify_subscribers(&spot_resource);
+		printf("I did it!\n");
+		return;
+	}
+
+	if(status.free != STATUS_FREE)
+	{
+		printf("Not modified!\n");
+		REST.set_response_status(response, REST.status.NOT_MODIFIED);
+		return;
+	}
+
+	// Getting user id and credit
 	len = REST.get_post_variable(request, "user", &value_buffer);
+	credit_len = REST.get_post_variable(request, "credit", &credit_buffer);
+
+	if(value_buffer[len-1] == '\n')
+		--len;
+	if(credit_buffer[credit_len-1] == '\n')
+		--credit_len;
 	
 	printf("Received user=`%s`.\n", value_buffer);
+	printf("Received credit=`%s`.\n", credit_buffer);
 	
-	// If the spot is busy, I need to update the user value
-	if (len < 1)
+	if (len < 1 || credit_len < 1)
 	{
 		printf("Invalid!\n");
 		REST.set_response_status(response, REST.status.BAD_REQUEST);
 		return;
 	}
 	
-	status.free = 0;
-	status.user = atoi(value_buffer);
+	// If the spot is busy, I need to update the user value
+	status.free = STATUS_WAITING;
+	strncpy(status.user, value_buffer, len);
+	status.user[len] = '\0';
+
+	strncpy(status.credit, credit_buffer, credit_len);
+	status.credit[credit_len] = '\0';
 	
-	printf("Assigned to user %d!\n", status.user);
+	printf("Spot is waiting for response for user %s and credit card %s!\n", status.user, status.credit);
 	
 	REST.set_response_status(response, REST.status.CHANGED);
 	spot_get_handler(request, response, buffer, preferred_size, offset);
@@ -126,8 +180,8 @@ PROCESS_THREAD(server, ev, data){
 	PROCESS_BEGIN();
 	rest_init_engine();
 	
-	status.free = 1;
-	status.user = -1;
+	status.free = STATUS_FREE;
+	strcpy(status.user, "null");
 
 	rest_activate_resource(&spot_resource, "park_spot");
 	
